b25f590d33545b67a46cf8314bde1b5c7187e543
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2024-01-30 19:47:38 +0800

    grid

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import PercolationStats from './MonteCarloSim';
+import PercolationStats from './PercolationStats';
 import readline from 'readline-sync';
+import Percolation from './Percolation';
 
 const n = readline.questionInt('Enter the size of the grid: ');
 const trials = readline.questionInt('Enter the number of trials: ');
 
-const percolationStats = new PercolationStats(n, trials);
+const grid = () => {
+  let percent = 0;
+  const percolation = new Percolation(n);
+  const percolationStats = new PercolationStats(n, trials);
+
+  while (!percolation.percolates()) {
+    percolation.percolateChance(percent);
+    percolation.displayGrid();
+    console.log('\n')
+    percent += 10;
+
+    if (percent === 100) {
+      break;
+    }
+  }
+
+  console.log('percentage until percolated: ', + percent)
   console.log('Mean: ', + percolationStats.getMean())
   console.log('Standard Deviation: ', percolationStats.getSD())
   console.log('High Confidence: ', + percolationStats.getHighConfidence())
   console.log('Low Confidence: ', + percolationStats.getLowConfidence())
+}
+
+grid();
